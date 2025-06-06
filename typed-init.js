e65c702c1cc545ec2a912f972b608053c51d4cf6
AmiUnadkat <amiunadkat2505@gmail.com>
2025-06-06 21:15:18 +0530

    Initial portfolio commit

---
+++ typed-init.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typeData = new Typed(".role", {
+      strings: [
+        "Student",
+        "Web Developer",
+        "Fronten Developer",
+        "Coder",
+      ],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1000,
+    });
+  });
